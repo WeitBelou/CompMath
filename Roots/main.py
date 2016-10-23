bdedc5834b8baa4294f74a9b583d0342e0547111
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-10-23 15:29:28 +0300

    Set up problem solver.

--- Roots/main.py
+++ Roots/main.py
@@ -1,12 +1,17 @@
-import numpy as np
-
-import Roots.number_estimators as rne
-import Roots.root_approximators as ra
-import Roots.polynomial_solver as r
+from Roots.parameters import Parameters
+from Roots.problem_solver import ProblemSolver
+from Roots.number_estimators import cartesian, fourier, sturm
+from Roots.root_approximators import binomial_search, newton
+from Roots.polynomial_solver import PolynomialSolver
 
 if __name__ == '__main__':
-    solver = r.PolynomialSolver(rne.sturm,
-                                ra.binomial_search)
-    answer = solver.solve(np.poly1d([1, -1, 3, -4], True), 1e-6)
+    parameters = Parameters()
+    parameters.parse_file('parameters.txt')
+
+    solver = ProblemSolver(parameters)
+
+    poly_solver = PolynomialSolver(sturm, binomial_search)
+
+    answer = solver.solve(poly_solver, 1e-3)
 
     print(answer)
